149416526f4e3a8f8f4e7efcc1baf768e73434f4
cmbrown18-rrd <christopher.brown@rrd.com>
2025-04-17 10:52:40 -0400

    Update streamlit_app.py
    indenting further

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ if ingredients_list:
 
         st.success('Your Smoothie is ordered, '+name_on_order+ '!', icon="✅")
 
-  # New section to display smoothiefroot nutrition information
-  import requests
-  smoothiefroot_response = request.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-  st.text(smoothiefroot_response)
+    # New section to display smoothiefroot nutrition information
+    import requests
+    smoothiefroot_response = request.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.text(smoothiefroot_response)
